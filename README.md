e9b1dfb7fa5884e0d66e7c50ed47cbbe4195b250
wagenaartje <wagenaartje@protonmail.com>
2017-08-30 14:03:05 +0200

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,162 @@
-# quickdraw.js
-The Google Quick Draw dataset ported to a MNIST-like 28x28 greyscale image, useable by JavaScript machine-learning libraries
+![](https://quickdraw.withgoogle.com/static/shareimg.png)
+
+### quickdraw.js
+
+<hr>
+
+This library allows you to import any of the millions of drawings from the
+[Google Quick Draw dataset](https://github.com/googlecreativelab/quickdraw-dataset)
+and to use them with JavaScript machine learning libraries like
+[Synaptic](https://github.com/cazala/synaptic) and
+[Neataptic](https://github.com/wagenaartje/neataptic). At this moment, this
+library is not supported by the browser due to size issues.
+
+**Example usage** (using Neataptic)
+
+```js
+const dataSet = quickDraw.set(100, ['car', 'airplane']);
+const network = new neataptic.architect.Perceptron(dataSet.input, 20, dataSet.output);
+
+network.train(dataSet.set, {
+  iterations: 100,
+  log: 1,
+  rate: 0.1
+});
+```
+
+This example will teach a neural network to distinguish if a drawing is either
+a car or a plane, using a dataset size of 100.
+
+## Getting started
+Installing `quickdraw.js` is easy with `npm`, just run:
+
+```batch
+npm install quickdraw.js
+```
+
+There is a high chance you will have a problem during the installation of the
+`canvas` library, which is a dependency. Please follow the steps
+[here](https://github.com/Automattic/node-canvas/wiki) and it should fix any
+problems.
+
+Then, in your `Node.js` script, require the library:
+
+```js
+var quickDraw = require('quickdraw.js');
+```
+
+## Usage
+Usage is fairly easy, but keep in mind that the Google Quick Draw dataset is
+a very large dataset. So before going mayhem on downloading, take into account
+that each _sample_ is a little more than `2kb` (for 28x28).
+
+With about 100000 samples for each of the 345 categories, that would take
+gigabytes of space.
+
+_[dev note] i'm looking into compressing and decompressing :)_
+
+### quickDraw.set
+This allows you to get a dataset that is compatible with Synaptic and Neataptic.
+Default usage:
+
+```js
+quickDraw.set(size, categories)
+```
+
+Where `size` is the total amount of samples you want in your dataset. The amount
+of samples will be distributed among the categories the best way possible.
+
+The `categories` parameter is optional. If none is given, samples of each
+category will be present. When given, it should be an array containing the
+names of the categories you want to include.
+[See the full list here](https://github.com/googlecreativelab/quickdraw-dataset/blob/master/categories.txt).
+
+All given categories should have the same sample dimensions!
+
+Example:
+
+```js
+var set = quickDraw.set(600, ['car', 'airplane', 'bicycle'])
+```
+
+This example will return a dataset with 600 samples, containing 200 samples from
+each of the 3 given categories.
+
+The set function returns an object which is built up like this:
+
+```js
+{
+  set: [
+    { input: [0, 0, 0, 0.418, 0...], output: [1, 0, 0]},
+    { input: [0, 0, 0, 0, 0, 0, 0...], output: [0, 1, 1]},
+    { input: [0.156, 0, 0, 0.163, // one object for every sample
+  ],
+  output: 3, // amount of categories
+  input: 784 // dimension of samples (28 x 28)
+}
+```
+
+The `.set` property should be passed to Synaptic or Neataptic as the dataset.
+The `input` and `output` parameters are useful for setting up the input/output
+size of the neural network.
+
+### quickDraw.importAll <sup>async<sup>
+By default, this library comes with 100 samples of each category. Each of these
+samples is a 784 (28x28 image) array containing values in the range of `0-1`,
+where `1` indicates 'fully drawn'.
+
+If you want to change the amount of samples per category, or the dimensions
+of the dataset, use this function the following way:
+
+```js
+quickDraw.importAll(samplesPerCategory, dimension);
+```
+
+So for example, calling:
+
+```js
+quickDraw.importAll(250, 64);
+```
+
+Would download, convert and save 250 samples per category as a 64x64 greyscale
+array.
+
+### quickDraw.import <sup>async<sup>
+This is similar to `quickDraw.importAll`, but this function will just update
+the samples of óne category. The usage is fairly simple:
+
+```js
+quickDraw.import(category, amount, size);
+```
+
+* The `category` parameter is the string name of the category that you want
+to update. [See the full list here](https://github.com/googlecreativelab/quickdraw-dataset/blob/master/categories.txt).
+* The `amount` parameter is the amount of samples that should be downloaded for
+the given category.
+* The `size` parameter is optional, the default is `28` (28x28).
+
+Example:
+
+```js
+quickDraw.import('broccoli', 400);
+```
+
+Will download, convert and save 400 28x28 samples from the `brocolli` category.
+
+## Contributing
+Feel free to contribute! Keep in mind that this project is fairly new, and I
+have some things in my head that I want to implement. So before you create a PR
+to work on a new feature, create an issue so we can discuss the feature first.
+
+You can also participate in the discussions at the [issues
+section](https://github.com/wagenaartje/stocks.js/issues), every opinion we can
+get is useful.
+
+## Further notices
+This data made available by Google, Inc. under the Creative Commons Attribution 4.0 International license.
+https://creativecommons.org/licenses/by/4.0/
+
+<hr>
+
+You made it all the way down! If you appreciate this repo and want to support the development of it, please consider donating :thumbsup:
+[![Donate](https://img.shields.io/badge/Donate-PayPal-green.svg)](https://www.paypal.com/cgi-bin/webscr?cmd=_s-xclick&hosted_button_id=CXS3G8NHBYEZE)
---
+++ src/quickdraw.js
@@ -0,0 +1,202 @@
+const fs = require('fs');
+const path = require('path');
+const https = require('https');
+const ndjson = require('ndjson');
+const JSONStream = require('JSONStream');
+const { createCanvas } = require('canvas');
+
+/*******************************************************************************
+                                  QUICKDRAW
+*******************************************************************************/
+
+const quickDraw = {
+  /** Converts the strokes of a drawing to an actual canvas */
+  _strokeToCanvas: function (data, size) {
+    var canvas = createCanvas(size, size);
+
+    var ctx = canvas.getContext('2d');
+
+    for (var i = 0; i < data.length; i++) {
+      var stroke = data[i];
+      ctx.moveTo(stroke[0][0] * size / 256, stroke[1][0] * size / 256);
+      for (var j = 0; j < stroke[0].length; j++) {
+        ctx.lineTo(stroke[0][j] * size / 256, stroke[1][j] * size / 256);
+      }
+    }
+
+    ctx.stroke();
+
+    return { canvas: canvas, context: ctx };
+  },
+
+  /** Converts a canvas to a greyscale size x size array */
+  _contextToArray: function (context, size) {
+    var pixelData = context.getImageData(0, 0, size, size).data;
+
+    var result = [];
+    for (var i = 3; i < pixelData.length; i += 4) {
+      let greyScale = Math.round(pixelData[i] / 256 * 1000) / 1000;
+      result.push(greyScale);
+    }
+
+    return result;
+  },
+
+  /** Downloads the given amount of drawings from the given category */
+  _downloadSet: function (category, amount) {
+    var url = 'https://storage.googleapis.com/quickdraw_dataset/full/simplified/';
+    url += encodeURIComponent(category) + '.ndjson';
+
+    return new Promise((resolve, reject) => {
+      https.get(url, (res) => {
+        var { statusCode } = res;
+
+        if (statusCode !== 200) {
+          throw new Error(`Request Failed.\n Status Code: ${statusCode}`);
+        }
+
+        res.setEncoding('utf8');
+        var drawings = [];
+        res
+          .pipe(ndjson.parse())
+          .on('data', function (obj) {
+            if (drawings.length < amount) {
+              drawings.push(obj);
+            } else {
+              this.destroy();
+            }
+          })
+          .on('end', () => {
+            resolve(drawings);
+          })
+          .on('close', () => {
+            resolve(drawings);
+          });
+      }).on('error', (e) => {
+        throw new Error(e.message);
+      });
+    });
+  },
+
+  /** Imports the dataset of the certain category  */
+  import: async function (category, amount, size = 28) {
+    var drawings = await quickDraw._downloadSet(category, amount);
+
+    if (drawings.length < amount) {
+      console.warn(`Requested ${amount} images from '${category}', only ${drawings.length} available!`);
+    }
+
+    var fileName = category + '.json';
+    var transformStream = JSONStream.stringify('{ "data": [', ',', ']}');
+    var outputStream = fs.createWriteStream(path.join(__dirname, '/drawings/', fileName));
+
+    transformStream.pipe(outputStream);
+
+    drawings.forEach(function (d) {
+      let { context } = quickDraw._strokeToCanvas(d.drawing, size);
+      let array = quickDraw._contextToArray(context, size);
+      transformStream.write(array);
+    });
+
+    transformStream.end();
+
+    outputStream.on('finish', function handleFinish () {
+      console.log(category, ' - Processing done! # of drawings:', drawings.length);
+    });
+  },
+
+  /** Imports the datasets of all categories */
+  importAll: async function (amount, size) {
+    var categories = require('./categories');
+
+    for (var i = 0; i < categories.length; i++) {
+      let category = categories[i];
+      await quickDraw.import(category, amount, size);
+    }
+  },
+
+  /** Returns a useable dataset for Neataptic and Synaptic */
+  set: function (amount, categories = require('./categories')) {
+    var dataSet = [];
+    var chunkSize = Math.floor(amount / categories.length);
+
+    var rest = (amount / categories.length - chunkSize) * categories.length;
+
+    var inputSize;
+
+    for (var i = 0; i < categories.length; i++) {
+      let category = categories[i];
+
+      let data;
+      try {
+        data = require(`./drawings/${category}.json`).data;
+      } catch (err) {
+        throw new Error(`Missing category: '${category}'. Please import!`);
+      }
+
+      if (i === 0) {
+        inputSize = data[0].length;
+      } else if (data[0].length !== inputSize) {
+        console.warn(`${category} set does not have correct dimensions and has not been included. Wanted dim: ${Math.sqrt(inputSize)}, ${category} dim: ${Math.sqrt(data[0].length)}`);
+      }
+
+      let toPick;
+      if (rest > 0) {
+        toPick = chunkSize + 1;
+        rest--;
+      } else {
+        toPick = chunkSize;
+      }
+
+      if (data.length < toPick) {
+        throw new Error(`Too few elements in local '${category}' set! (${data.length}, ${amount} needed)`);
+      }
+
+      let picked = quickDraw._pickRandom(data, toPick);
+
+      for (var j = 0; j < picked.length; j++) {
+        let output = new Array(categories.length).fill(0);
+        output[i] = 1;
+
+        dataSet.push({ input: picked[j], output: output });
+      }
+    }
+
+    dataSet = quickDraw._shuffle(dataSet);
+    return { input: inputSize, set: dataSet, output: categories.length };
+  },
+
+  /** Pick non-repeating elements in a random fashion */
+  _pickRandom: function (array, amount) {
+    var picked = [];
+    for (var i = array.length - 1; i >= array.length - amount; i--) {
+      let j = Math.floor(Math.random() * (i + 1));
+
+      let pick = array[j];
+      let temp = array[i];
+
+      array[i] = pick;
+      array[j] = temp;
+
+      picked.push(pick);
+    }
+
+    return picked;
+  },
+
+  /** Shuffles an array */
+  _shuffle: function (array) {
+    var i = array.length;
+
+    while (--i) {
+      let j = Math.floor(Math.random() * (i + 1));
+      let temp = array[i];
+      array[i] = array[j];
+      array[j] = temp;
+    }
+
+    return array;
+  }
+};
+
+module.exports = quickDraw;
