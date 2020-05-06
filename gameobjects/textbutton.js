7982b2dcace6ce08c12451769cface5d14c655a6
Stephen Thompson <s.thompson@ucl.ac.uk>
2020-05-06 08:28:08 +0100

    Issue #2 added button logic

--- gameobjects/textbutton.js
+++ gameobjects/textbutton.js
@@ -1,15 +1,29 @@
 class TextButton extends Phaser.GameObjects.Text {
 
-    constructor(scene, x, y, text, style, callback) {
+    constructor(scene, x, y, text, style, target_text, on_success, on_fail ) {
      super(scene, x, y, text, style);
 
      this.setInteractive({ useHandCursor: true })
 	.on('pointerover', () => this.enterButtonHoverState() )
+	.on('pointerout', () => this.enterButtonResetState() )
+	.on('pointerdown', () => this.checkanswer(target_text, on_success, on_fail) )
     }
 
     enterButtonHoverState() {
       this.setStyle({ fill: '#ff0'});
-      console.log(this.text);
+    }
+    enterButtonResetState() {
+      this.setStyle({ fill: '#0f0'});
+    }
+
+    checkanswer(target_text, on_success, on_fail) {
+      if ( this.text == target_text) {
+	      on_success();
+      }
+      else
+      {
+	  on_fail();
+      }
     }
 }
 
